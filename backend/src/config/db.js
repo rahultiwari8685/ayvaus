0163fa6ea7fb42b7fad6f6d71bd2ff1a15ca3e1d
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-01 14:39:54 +0530

    env and db updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.log(err);
+    console.log("❌ DB ERROR:", err);
   }
 };
