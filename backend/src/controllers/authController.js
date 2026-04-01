9ca203f43b3f48874fda2883199b46410ca0f422
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-01 17:13:45 +0530

    authController added in backend

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  res.json({
+    message: "Register API working ✅",
+  });
+};
+
+export const login = (req, res) => {
+  res.json({
+    message: "Login API working ✅",
+  });
+};
