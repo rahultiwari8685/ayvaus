40fe020975cc2de42420e93267ae58aca6642c89
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-12 14:10:40 +0530

    login updated

--- frontend/app/serious/login/page.js
+++ frontend/app/serious/login/page.js
@@ -43,7 +43,7 @@ export default function Login() {
 
         // ✅ Smart redirect
         if (data.profileComplete) {
-          router.push("/serious/video");
+          router.push("/serious/match");
         } else {
           router.push("/serious/profile");
         }
