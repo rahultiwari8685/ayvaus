4bd882d1c6ca991161dc5d6026d49c66e2944b37
rahultiwari8685 <rtiwari69572@gmail.com>
2026-05-03 16:10:16 +0530

    update

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -101,7 +101,7 @@ export default function HomePage() {
               </p> */}
 
               {/* Stats */}
-              <div className="grid grid-cols-3 gap-4 mt-50 max-w-2xl">
+              <div className="grid grid-cols-3 gap-4 mt-40 max-w-2xl">
                 <div className="rounded-2xl border border-white/10 bg-black/30 backdrop-blur-xl p-5">
                   <h3 className="text-3xl font-bold text-pink-400">1k+</h3>
 
