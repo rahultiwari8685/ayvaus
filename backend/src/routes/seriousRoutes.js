3650cae88193db8dd03e86dbf52337e45af24efb
rahultiwari8685 <rtiwari69572@gmail.com>
2026-05-26 13:56:32 +0530

    auth issue resolved

--- backend/src/routes/seriousRoutes.js
+++ backend/src/routes/seriousRoutes.js
@@ -3,13 +3,12 @@ import { loginUser } from "../controllers/authController.js";
 import { registerUser } from "../controllers/registerUser.js";
 import { giveDailyReward } from "../controllers/giveDailyReward.js";
 import getWallet from "../controllers/getWallet.js";
-import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/daily-reward", auth, giveDailyReward);
-router.get("/wallet", auth, getWallet);
+router.post("/daily-reward", loginUser, giveDailyReward);
+router.get("/wallet", loginUser, getWallet);
 
 export default router;
