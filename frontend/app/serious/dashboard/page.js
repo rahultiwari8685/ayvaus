15058e79ff91475e5829971153278a866ebfb01b
rahultiwari8685 <rtiwari69572@gmail.com>
2026-07-16 15:51:36 +0530

    updated

--- frontend/app/serious/dashboard/page.js
+++ frontend/app/serious/dashboard/page.js
@@ -178,7 +178,7 @@ export default function SeriousDashboard() {
     const token = localStorage.getItem("token");
 
     if (!token) {
-      router.replace("/serious/login");
+      router.replace("/");
     }
   }, [router]);
 
