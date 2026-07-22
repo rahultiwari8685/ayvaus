55db2d943a9c5eb5b84d1f263c2b7d31d1dcf485
rahultiwari8685 <rtiwari69572@gmail.com>
2026-07-22 12:17:34 +0530

    debug plain issue

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
 
