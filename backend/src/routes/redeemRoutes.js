5121854e6356ee721c406788ff9a13edbb043b3e
rahultiwari8685 <rtiwari69572@gmail.com>
2026-07-22 16:07:16 +0530

    added

--- backend/src/routes/redeemRoutes.js
+++ backend/src/routes/redeemRoutes.js
@@ -3,34 +3,18 @@ import express from "express";
 import {
   createRedeemRequest,
   redeemHistory,
-  approveRedeem,
-  rejectRedeem,
-  markPaid,
-  getAllRedeemRequests,
   getRedeemById,
 } from "../controllers/redeemController.js";
 
 import { auth } from "../middlewares/auth.js";
-import { adminProtect } from "../middlewares/admin.js";
 
 const router = express.Router();
 
-/* USER */
-
+// User Routes
 router.post("/", auth, createRedeemRequest);
 
 router.get("/history", auth, redeemHistory);
 
 router.get("/:id", auth, getRedeemById);
-
-/* ADMIN */
-
-router.get("/admin/all", auth, adminProtect, getAllRedeemRequests);
-
-router.put("/admin/approve/:id", auth, adminProtect, approveRedeem);
-
-router.put("/admin/reject/:id", auth, adminProtect, rejectRedeem);
-
-router.put("/admin/paid/:id", auth, adminProtect, markPaid);
 
 export default router;
