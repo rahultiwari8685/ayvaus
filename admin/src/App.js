cfbb87f050372cc226e41d8e53195b9d97ecedd6
rahultiwari8685 <rtiwari69572@gmail.com>
2026-07-24 12:00:42 +0530

    register remove

--- admin/src/App.js
+++ admin/src/App.js
@@ -14,7 +14,7 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'))
-const Register = React.lazy(() => import('./views/pages/register/Register'))
+// const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
@@ -70,7 +70,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" name="Login Page" element={<Login />} />
           <Route exact path="/login" name="Login Page" element={<Login />} />
-          <Route exact path="/register" name="Register Page" element={<Register />} />
+          {/* <Route exact path="/register" name="Register Page" element={<Register />} /> */}
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
           <Route path="*" name="Home" element={<DefaultLayout />} />
