cd244dea2df254bfa595298863f4776cbd015e55
rahultiwari8685 <rtiwari69572@gmail.com>
2026-07-24 12:11:36 +0530

    update

--- admin/src/_nav.js
+++ admin/src/_nav.js
@@ -27,7 +27,7 @@ export const navigation = [
   {
     component: CNavItem,
     name: 'Redeem Requests',
-    to: '/redeemRequests',
+    to: '/redeemRequest',
     icon: <CIcon icon={cilDollar} customClassName="nav-icon" />,
   },
   {
