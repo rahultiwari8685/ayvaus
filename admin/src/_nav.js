ef3153f6da0ef390af407ab8e2b65c9222da7039
rahultiwari8685 <rtiwari69572@gmail.com>
2026-09-23 13:23:35 +0530

    added

--- admin/src/_nav.js
+++ admin/src/_nav.js
@@ -34,5 +34,6 @@ export const navigation = [
     component: CNavItem,
     name: 'Corporate Verification',
     to: '/corporate/verification',
+    icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
   },
 ]
